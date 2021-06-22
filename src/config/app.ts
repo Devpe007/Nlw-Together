64000e45f550949dedafbef7a9f0f8a38fadefd4
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-06-22 20:51:18 -0300

    created error handling

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import '../database';
 
 import routes from '../routes';
+import middlewareError from '../middlewares/error/Error';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(middlewareError);
 
 export default app;
